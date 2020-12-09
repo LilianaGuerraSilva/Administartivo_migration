3c3e345ee27086dbb42fa8e684def32833f30e0d
romny.lopez <romny.lopez@galac.com>
2020-12-09 10:05:57 -0400

    Se ajustan assemblyInfo y proyects de vba para actualizacion a nuevas versiones de librerias

--- source/Galac.Adm.Brl.CAnticipo/Properties/AssemblyInfo.cs
+++ source/Galac.Adm.Brl.CAnticipo/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Número de compilación
 //      Revisión
 //
-[assembly: AssemblyVersion("2.35.0.0")]
-[assembly: AssemblyFileVersion("2.35.0.0")]
+[assembly: AssemblyVersion("2.50.0.0")]
+[assembly: AssemblyFileVersion("2.50.0.0")]
