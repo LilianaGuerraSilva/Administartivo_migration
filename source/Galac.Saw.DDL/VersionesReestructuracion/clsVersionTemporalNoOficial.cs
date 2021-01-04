4e2f36e9b9f3eb4f80088e74deab49a2807f67d3
romny.lopez <romny.lopez@galac.com>
2021-01-04 14:13:45 -0400

    se hace correccion por reestructuracion para reinstalar registros faltantes de elemento del costo luego de restaurar una compania, se ajusta nivel de version de bd por reestrcturacion, se ajusta maximo longitud de decimales a 10 digitos para items y pagos en ipresora fiscal epson.

--- source/Galac.Saw.DDL/VersionesReestructuracion/clsVersionTemporalNoOficial.cs
+++ source/Galac.Saw.DDL/VersionesReestructuracion/clsVersionTemporalNoOficial.cs
@@ -3,22 +3,17 @@ using System.Data;
 using LibGalac.Aos.Base;
 using LibGalac.Aos.DefGen;
 using Galac.Saw.Ccl.SttDef;
+using System.Threading;
 
 namespace Galac.Saw.DDL.VersionesReestructuracion {
 
     class clsVersionTemporalNoOficial:clsVersionARestructurar {
         public clsVersionTemporalNoOficial(string valCurrentDataBaseName) : base(valCurrentDataBaseName) { }
         public override bool UpdateToVersion() {
-            StartConnectionNoTransaction();
-            AgregarParametroUsaDivisaComoMonedaPrincipalDeIngresoDeDatos();
+            StartConnectionNoTransaction();           
             DisposeConnectionNoTransaction();
             return true;
-        }
-
-        private void AgregarParametroUsaDivisaComoMonedaPrincipalDeIngresoDeDatos() {
-            AgregarNuevoParametro("UsaDivisaComoMonedaPrincipalDeIngresoDeDatos", "Bancos", 7, "7.2-Moneda", 2, "", eTipoDeDatoParametros.String, "", 'N', "N");
-        }
-
+        }      
     }
 }
 
