6053e69d1156c134e9fb31609537db08347e8bff
Romny López <romny.lopez@galac.com>
2021-03-11 16:23:43 -0400

    Versionamiento a nueva versión de librerias v5.13.0.0

--- source/Galac.Saw.Brl.Integracion/clsDespro.cs
+++ source/Galac.Saw.Brl.Integracion/clsDespro.cs
@@ -6,8 +6,7 @@ using System.Text;
 namespace Galac.Saw.Brl.Integracion {
     public class clsDespro {
         public string Version() {
-            return "2.35.0.0";
-            //return "2.10.1.0";
+            return "2.50.0.0";           
         }
     }
 }
