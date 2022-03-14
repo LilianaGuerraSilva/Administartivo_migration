6482d3e227c36e5681f62d918bb35e79508b080b
rafael.colina <galac.softbb@galac.com>
2022-03-14 09:57:33 -0400

    ReformaITF_4

--- source/Galac.Adm.Ccl.Banco/ICuentaBancariaPdn.cs
+++ source/Galac.Adm.Ccl.Banco/ICuentaBancariaPdn.cs
@@ -5,15 +5,14 @@ using System.Text;
 using LibGalac.Aos.Base;
 
 namespace Galac.Adm.Ccl.Banco {
-    public interface ICuentaBancariaPdn: ILibPdn {
-        void GeneraCuentaBancariaGenericaSiCiaNoGeneraMovimientosBancarios(int vfwConsecutivoCompania, int vfwCodigoBanco, string valCodigoMonedaLocal, string valNombreMonedaLocal);
-        void InsertaCuentaBancariaGenericaSiHaceFalta(int vfwConsecutivoCompania, int vfwCodigoBanco, string valCodigoMonedaLocal, string valNombreMonedaLocal);
-        bool ActualizaSaldoDisponibleEnCuenta(int vfwlConsecutivoCompania, string vfwCodigoCuenta, string vfwMonto, string vfwIngresoEgreso, int vfwmAction, string vfwMontoOriginal, bool vfwSeModificoTipoConcepto);
-        bool RecalculaSaldoCuentasBancarias(int valConsecutivoCompania);
-        string GetCuentaBancariaGenericaPorDefecto();
-        bool TieneMovimientosBancariosDeReposicionCajaChica(int valConsecutivoCompania, string valCodigoCuentaBancaria);
-        bool EsValidaCuentaBancariaCajaChica(int valConsecutivoCompania, string valCodigoCuentaBancaria);
-        bool ExistenMovimientosCuentaBancaria(int valConsecutivoCompania);
-    }
-    
+	public interface ICuentaBancariaPdn : ILibPdn {
+		void GeneraCuentaBancariaGenericaSiCiaNoGeneraMovimientosBancarios(int vfwConsecutivoCompania, int vfwCodigoBanco, string valCodigoMonedaLocal, string valNombreMonedaLocal);
+		void InsertaCuentaBancariaGenericaSiHaceFalta(int vfwConsecutivoCompania, int vfwCodigoBanco, string valCodigoMonedaLocal, string valNombreMonedaLocal);
+		bool ActualizaSaldoDisponibleEnCuenta(int vfwlConsecutivoCompania, string vfwCodigoCuenta, string vfwMonto, string vfwIngresoEgreso, int vfwmAction, string vfwMontoOriginal, bool vfwSeModificoTipoConcepto);
+		bool RecalculaSaldoCuentasBancarias(int valConsecutivoCompania);
+		string GetCuentaBancariaGenericaPorDefecto();
+		bool TieneMovimientosBancariosDeReposicionCajaChica(int valConsecutivoCompania, string valCodigoCuentaBancaria);
+		bool EsValidaCuentaBancariaCajaChica(int valConsecutivoCompania, string valCodigoCuentaBancaria);
+		bool ExistenMovimientosCuentaBancaria(int valConsecutivoCompania);
+	}
 }
--- source/Galac.Saw.DDL/VersionesReestructuracion/clsVersionTemporalNoOficial.cs
+++ source/Galac.Saw.DDL/VersionesReestructuracion/clsVersionTemporalNoOficial.cs
@@ -1,39 +1,47 @@
-﻿﻿using System.Text;
+﻿using System.Text;
 using System.Data;
 using LibGalac.Aos.Base;
 using LibGalac.Aos.DefGen;
 using Galac.Saw.Ccl.SttDef;
 using System.Threading;
 using LibGalac.Aos.Dal;
-
+using System;
 
 namespace Galac.Saw.DDL.VersionesReestructuracion {
-
-    class clsVersionTemporalNoOficial:clsVersionARestructurar {
-        public clsVersionTemporalNoOficial(string valCurrentDataBaseName) : base(valCurrentDataBaseName) { }
-        public override bool UpdateToVersion() {
-            StartConnectionNoTransaction();
-            CreacionCampoEnMovBancario();
+	class clsVersionTemporalNoOficial : clsVersionARestructurar {
+		public clsVersionTemporalNoOficial(string valCurrentDataBaseName) : base(valCurrentDataBaseName) { }
+		public override bool UpdateToVersion() {
+			StartConnectionNoTransaction();
+			CreacionCampoEnMovBancario();
 			CrearNuevosCamposImpTransacBancarias();
-            DisposeConnectionNoTransaction();            
-            return true;
-        }
+			AgregaCamposCuentaBancaria();
+			DisposeConnectionNoTransaction();
+			return true;
+		}
 
-        private void CreacionCampoEnMovBancario() {
-            AddColumnNumeric("dbo.MovimientoBancario", "AlicuotaImpBancario", 12, 2, "", 0);                 
-        }
+		private void CreacionCampoEnMovBancario() {
+			AddColumnNumeric("dbo.MovimientoBancario", "AlicuotaImpBancario", 12, 2, "", 0);
+		}
 
-        private void CrearNuevosCamposImpTransacBancarias(){
-            if (AddColumnDecimal("Adm.ImpTransacBancarias", "AlicuotaC1Al4", 25, 4, "", 0)){
-                AddDefaultConstraint("Adm.ImpTransacBancarias", "d_ImpBanAlC14", "0", "AlicuotaC1Al4");
-            }
-            if (AddColumnDecimal("Adm.ImpTransacBancarias", "AlicuotaC5", 25, 4, "", 0)){
-                AddDefaultConstraint("Adm.ImpTransacBancarias", "d_ImpBanAlC5", "0", "AlicuotaC5");
-            }
-            if (AddColumnDecimal("Adm.ImpTransacBancarias", "AlicuotaC6", 25, 4, "", 0)){
-                AddDefaultConstraint("Adm.ImpTransacBancarias", "d_ImpBanAlC6", "0", "AlicuotaC6");
-            }
-        }
-    }
+		private void CrearNuevosCamposImpTransacBancarias() {
+			if (AddColumnDecimal("Adm.ImpTransacBancarias", "AlicuotaC1Al4", 25, 4, "", 0)) {
+				AddDefaultConstraint("Adm.ImpTransacBancarias", "d_ImpBanAlC14", "0", "AlicuotaC1Al4");
+			}
+			if (AddColumnDecimal("Adm.ImpTransacBancarias", "AlicuotaC5", 25, 4, "", 0)) {
+				AddDefaultConstraint("Adm.ImpTransacBancarias", "d_ImpBanAlC5", "0", "AlicuotaC5");
+			}
+			if (AddColumnDecimal("Adm.ImpTransacBancarias", "AlicuotaC6", 25, 4, "", 0)) {
+				AddDefaultConstraint("Adm.ImpTransacBancarias", "d_ImpBanAlC6", "0", "AlicuotaC6");
+			}
+		}
+
+		private void AgregaCamposCuentaBancaria() {
+			if (AddColumnEnumerative("Saw.CuentaBancaria", "TipoDeAlicuotaPorContribuyente","",0)) {
+				AddDefaultConstraint("Saw.CuentaBancaria", "d_CueBanTiDeAlPoCo", "'0'", "TipoDeAlicuotaPorContribuyente");
+			}
+			if (AddColumnBoolean("Saw.CuentaBancaria", "GeneraMovBancarioPorIGTF", "", false)) {
+				AddNotNullConstraint("Saw.CuentaBancaria", "GeneraMovBancarioPorIGTF", InsSql.CharTypeForDb(1));
+			}
+		}
+	}
 }
-
