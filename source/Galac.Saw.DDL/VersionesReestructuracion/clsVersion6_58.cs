9e8acb7758851fba9070b7d2a9afea863a940e1a
erikson.didik <galac.softbb@galac.com>
2022-07-04 14:36:12 -0400

    Se suben cambio relacionado a SASEmprendedor

---
+++ source/Galac.Saw.DDL/VersionesReestructuracion/clsVersion6_58.cs
@@ -0,0 +1,31 @@
+﻿using LibGalac.Aos.Base;
+using Galac.Saw.Ccl.SttDef;
+using System.Threading;
+using System.Text;
+using LibGalac.Aos.Brl;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using System.Linq;
+using LibGalac.Aos.Base.Dal;
+using LibGalac.Aos.Dal;
+
+namespace Galac.Saw.DDL.VersionesReestructuracion {
+    class clsVersion6_58 : clsVersionARestructurar {
+        public clsVersion6_58(string valCurrentDataBaseName) : base(valCurrentDataBaseName) {
+            _VersionDataBase = "6.58";
+        }
+        public override bool UpdateToVersion() {
+            StartConnectionNoTransaction();
+            EliminarTasaDeCambioIgualCero();
+            DisposeConnectionNoTransaction();
+            return true;
+        }
+        private void EliminarTasaDeCambioIgualCero() {
+            QAdvSql insSql = new QAdvSql("");
+            StringBuilder vSql = new StringBuilder();
+            vSql.AppendLine("DELETE FROM comun.cambio ");
+            vSql.AppendLine("WHERE CambioAMonedaLocal = 0 ");
+            Execute(vSql.ToString(), 0);
+        }
+    }
+}
