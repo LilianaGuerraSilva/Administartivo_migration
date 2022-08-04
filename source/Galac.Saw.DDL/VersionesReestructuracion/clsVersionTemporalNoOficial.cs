836f2629b01d66bbb84fc7c86950d551c973aaf2
romny.lopez <galac.softbb@galac.com>
2022-08-04 08:42:43 -0400

    se quita codigo innecesario

--- source/Galac.Saw.DDL/VersionesReestructuracion/clsVersionTemporalNoOficial.cs
+++ source/Galac.Saw.DDL/VersionesReestructuracion/clsVersionTemporalNoOficial.cs
@@ -9,67 +9,15 @@ using System;
 using LibGalac.Aos.Base.Dal;
 
 namespace Galac.Saw.DDL.VersionesReestructuracion {
-    class clsVersionTemporalNoOficial :clsVersionARestructurar {
-        public clsVersionTemporalNoOficial(string valCurrentDataBaseName) : base(valCurrentDataBaseName) { }
-        public override bool UpdateToVersion() {
-            StartConnectionNoTransaction();
-            ActivaModoMejoradoPorDefecto();
+	class clsVersionTemporalNoOficial : clsVersionARestructurar {
+		public clsVersionTemporalNoOficial(string valCurrentDataBaseName) : base(valCurrentDataBaseName) { }
+		public override bool UpdateToVersion() {
+			StartConnectionNoTransaction();
 			ModificarTablasWincont();
 			ActualizaNombresInformes();
-			//CrearTablaTransferenciaEntreCuentasBancarias(); Nota: se Oculta temporalmente
-			//AgregaColumnasReglasDeContabilizacion();
 			DisposeConnectionNoTransaction();
-            return true;
-        }
-		
-        private void ActivaModoMejoradoPorDefecto() {
-            StringBuilder vSql = new StringBuilder();
-            vSql.AppendLine($"UPDATE Adm.Caja SET UsarModoDotNet={InsSql.ToSqlValue(true)}");
-            vSql.AppendLine("WHERE Consecutivo <> 0 ");
-            vSql.AppendLine($"AND FamiliaImpresoraFiscal IN({InsSql.ToSqlValue("0")},{InsSql.ToSqlValue("2")},{InsSql.ToSqlValue("3")})");
-            vSql.AppendLine($"AND UsaMaquinaFiscal ={InsSql.ToSqlValue(true)}");
-            Execute(vSql.ToString(), 0);
-        }
-
-		/*
-		
-		private void CrearTablaTransferenciaEntreCuentasBancarias() {
-			if (!TableExists("Adm.TransferenciaEntreCuentasBancarias")) {
-				new Galac.Adm.Dal.Banco.clsTransferenciaEntreCuentasBancariasED().InstalarTabla();
-			}
+			return true;
 		}
-		private void AgregaColumnasReglasDeContabilizacion() {
-			if (!ColumnExists("Saw.ReglasDeContabilizacion", "TipoContabilizacionTransfCtas")) {
-				if( AddColumnEnumerative("Saw.ReglasDeContabilizacion", "TipoContabilizacionTransfCtas", "", LibConvert.EnumToDbValue(0))) {
-					AddDefaultConstraint("Saw.ReglasDeContabilizacion", "d_RegDeConTiCoTrCt", "'0'", "TipoContabilizacionTransfCtas");
-                }
-				if (AddColumnEnumerative("Saw.ReglasDeContabilizacion", "ContabIndividualTransfCtas", "", LibConvert.EnumToDbValue(1))){
-					AddDefaultConstraint("Saw.ReglasDeContabilizacion", "d_RegDeConCoInTrCt", "'1'", "ContabIndividualTransfCtas");
-				}
-				if (AddColumnEnumerative("Saw.ReglasDeContabilizacion", "ContabPorLoteTransfCtas", "", LibConvert.EnumToDbValue(0))) {
-					AddDefaultConstraint("Saw.ReglasDeContabilizacion", "d_RegDeConCuTrCtBaDe", "'0'", "ContabPorLoteTransfCtas");
-				}
-				if (AddColumnString("Saw.ReglasDeContabilizacion", "CuentaTransfCtasBancoDestino", 30, "", "")) {
-					AddDefaultConstraint("Saw.ReglasDeContabilizacion", "d_RegDeConCoPoLoTrCt", _insSql.ToSqlValue(""), "CuentaTransfCtasBancoDestino");
-				}
-				if (AddColumnString("Saw.ReglasDeContabilizacion", "CuentaTransfCtasGastoComOrigen", 30, "", "")) {
-					AddDefaultConstraint("Saw.ReglasDeContabilizacion", "d_RegDeConCuTrCtGaCoOr", _insSql.ToSqlValue(""), "CuentaTransfCtasGastoComOrigen");
-				}
-				if (AddColumnString("Saw.ReglasDeContabilizacion", "CuentaTransfCtasGastoComDestino", 30, "", "")) {
-					AddDefaultConstraint("Saw.ReglasDeContabilizacion", "d_RegDeConCuTrCtGaCoDe", _insSql.ToSqlValue(""), "CuentaTransfCtasGastoComDestino");
-				}
-				if (AddColumnString("Saw.ReglasDeContabilizacion", "CuentaTransfCtasBancoOrigen", 30, "", "")){
-					AddDefaultConstraint("Saw.ReglasDeContabilizacion", "d_RegDeConCuTrCtBaOr", _insSql.ToSqlValue(""), "CuentaTransfCtasBancoOrigen");
-				}
-				if (AddColumnString("Saw.ReglasDeContabilizacion", "TransfCtasSigasTipoComprobante", 2, "", "")) {
-					AddDefaultConstraint("Saw.ReglasDeContabilizacion", "d_RegDeConTrCtSiTiCo", _insSql.ToSqlValue(""), "TransfCtasSigasTipoComprobante");
-				}
-				if (AddColumnBoolean("Saw.ReglasDeContabilizacion", "EditarComprobanteAfterInsertTransfCtas", "", false)) {
-					AddNotNullConstraint("Saw.ReglasDeContabilizacion", "EditarComprobanteAfterInsertTransfCtas", InsSql.CharTypeForDb(1)) ;
-				}
-			}
-		}
-		*/
 
 		private void ActualizaNombresInformes() {
 			string vSql = string.Empty;
@@ -97,10 +45,9 @@ namespace Galac.Saw.DDL.VersionesReestructuracion {
 			}
 		}
 
-
 		private void ModificarTablasWincont() {
-			string vSql = string.Empty;
-			if (ColumnExists("Contab.ParametrosConciliacion", "ExpresarBalancesEnDiferentesMonedas")) {
+			string vSql = string.Empty;			
+			if (ColumnExists("Contab.ParametrosConciliacion", "ExpresarBalancesEnDiferentesMonedas")) {				
 				vSql = vSql + "UPDATE Contab.ParametrosConciliacion SET ExpresarBalancesEnDiferentesMonedas = (CASE WHEN ExpresarBalancesEnDiferentesMonedas = 'N' THEN '" + (int)Galac.Contab.Ccl.WinCont.eExpresarBalancesEnMonedaExtrangera.NoAplicar + "' WHEN ExpresarBalancesEnDiferentesMonedas = 'S' THEN '" + (int)Galac.Contab.Ccl.WinCont.eExpresarBalancesEnMonedaExtrangera.ConDifCambiria + "' ELSE ExpresarBalancesEnDiferentesMonedas END) ";
 				Execute(vSql, 0);
 			}
