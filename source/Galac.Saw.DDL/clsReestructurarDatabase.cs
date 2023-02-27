5facabb29ef79b72095cf4a1ea41781aeabc5ad0
Jorge.Barillas <galac.softbb@galac.com>
2023-02-27 10:44:31 -0400

    ajustes

--- source/Galac.Saw.DDL/clsReestructurarDatabase.cs
+++ source/Galac.Saw.DDL/clsReestructurarDatabase.cs
@@ -696,6 +696,9 @@ namespace Galac.Saw.DDL {
                 if (HasToUpgradeToVersion("6.61")) {
                     vResult = vResult && new clsVersion6_61(_CurrentDataBaseName).UpdateToVersion();
                 }
+                if (HasToUpgradeToVersion("6.62")) {
+                    vResult = vResult && new clsVersion6_62(_CurrentDataBaseName).UpdateToVersion();
+                }
                 vResult = vResult && new clsVersionTemporalNoOficial(_CurrentDataBaseName).UpdateToVersion();
                 //vResult = vResult && CreateLostFields();
                 return vResult;
