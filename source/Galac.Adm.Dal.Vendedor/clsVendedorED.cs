bea3d0e9f71d5eed6b16428f9c828e5bd41692db
romny.lopez <galac.softbb@galac.com>
2023-07-11 13:55:20 -0400

    --

--- source/Galac.Adm.Dal.Vendedor/clsVendedorED.cs
+++ source/Galac.Adm.Dal.Vendedor/clsVendedorED.cs
@@ -125,7 +125,7 @@ namespace Galac.Adm.Dal.Vendedor {
             SQL.AppendLine("@ConsecutivoCompania" + InsSql.NumericTypeForDb(10, 0) + ",");
             SQL.AppendLine("@Consecutivo" + InsSql.NumericTypeForDb(10, 0) + ",");
             SQL.AppendLine("@Codigo" + InsSql.VarCharTypeForDb(5) + ",");
-            SQL.AppendLine("@Nombre" + InsSql.VarCharTypeForDb(35) + "= '',");
+            SQL.AppendLine("@Nombre" + InsSql.VarCharTypeForDb(35) + " = '',");
             SQL.AppendLine("@RIF" + InsSql.VarCharTypeForDb(20) + " = '',");
             SQL.AppendLine("@StatusVendedor" + InsSql.CharTypeForDb(1) + " = '0',");
             SQL.AppendLine("@Direccion" + InsSql.VarCharTypeForDb(255) + " = '',");
