9995a4861b530559d236eb2f8e5487499a5ac1cd
luis.garcia <luis.garcia@galac.com>
2023-08-22 15:57:57 -0400

    Se crea el proyecto IntegracionMS - Medios de Pago

---
+++ source/Galac.Adm.IntegracionMS.Venta/Galac.Adm.IntegracionMS.Venta.cls/GSC2PMegasoftView.xaml.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Galac.Adm.Uil.Venta.Views {
+    /// <summary>
+    /// Interaction logic for GSC2PMegasoftView.xaml
+    /// </summary>
+    public partial class GSC2PMegasoftView: UserControl {
+        #region Constructores
+
+        public GSC2PMegasoftView() {
+            InitializeComponent();
+        }
+        #endregion //Constructores
+
+
+    } //End of class GSC2PMegasoftView.xaml
+
+} //End of namespace Galac.Adm.Uil.Venta
+
