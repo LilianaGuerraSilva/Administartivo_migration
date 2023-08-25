7c384828bbdb5c9f6f997330e2deb7a08756d234
Jose.Colmenarez <galac.softbb@galac.com>
2023-08-25 10:10:04 -0400

    Clasese para el consumo de la api para procesar el pagomovil

---
+++ source/Galac.Adm.IntegracionMS.Venta/C2PMegasoftModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Galac.Adm.IntegracionMS.Venta {
+    namespace Preregister {
+        public class request {
+            public string cod_afiliacion { get; set; }
+        }
+
+        public class response {
+            public string codigo { get; set; }
+            public string descripcion { get; set; }
+            public string control { get; set; }
+        }
+    }
+
+    namespace ProcesarCambioPagoMovil {
+        public class request {
+            public string cod_afiliacion { get; set; }
+            public string control { get; set; }
+            public string cid { get; set; }
+            public string telefono { get; set; }
+            public string codigobanco { get; set; }
+            public string tipo_moneda { get; set; }
+            public string amount { get; set; }
+            public string factura { get; set; }
+        }
+
+        public class response {
+            public double control { get; set; }
+            public int codigo { get; set; }
+            public string descripcion { get; set; }
+            public object vtid { get; set; }
+            public object seqnum { get; set; }
+            public object authid { get; set; }
+            public object authname { get; set; }
+            public object factura { get; set; }
+            public object referencia { get; set; }
+            public object terminal { get; set; }
+            public object lote { get; set; }
+            public object rifbanco { get; set; }
+            public object afiliacion { get; set; }
+            public voucher voucher { get; set; }
+        }
+
+        public class voucher {
+            public string linea { get; set; }
+        }
+    }
+
+    namespace Querystatus {
+        public class request {
+            public string cod_afiliacion { get; set; }
+            public string control { get; set; }
+            public string cid { get; set; }
+            public double telefono { get; set; }
+            public string codigobanco { get; set; }
+            public string tipo_moneda { get; set; }
+            public string amount { get; set; }
+            public string factura { get; set; }
+        }
+
+        public class response {
+            public string control { get; set; }
+            public string cod_afiliacion { get; set; }
+            public string factura { get; set; }
+            public string monto { get; set; }
+            public string estado { get; set; }
+            public string codigo { get; set; }
+            public string descripcion { get; set; }
+            public string vtid { get; set; }
+            public string seqnum { get; set; }
+            public string authid { get; set; }
+            public string authname { get; set; }
+            public string tarjeta { get; set; }
+            public string referencia { get; set; }
+            public string terminal { get; set; }
+            public string lote { get; set; }
+            public string afiliacion { get; set; }
+            public string telefonoEmisor { get; set; }
+            public string tipotrx { get; set; }
+            public voucher voucher { get; set; }
+            public string text { get; set; }
+        }
+        public class voucher {
+            public string UT { get; set; }
+        }
+    }
+}
---
+++ source/Galac.Adm.IntegracionMS.Venta/C2PMegasoftNav.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Xml.Serialization;
+using System.Xml;
+using LibGalac.Aos.Base;
+
+namespace Galac.Adm.IntegracionMS.Venta {
+    public class C2PMegasoftNav {
+        string _UrlBase;
+        const string _Urlpreregistr = "payment/action/v2-preregistr";
+        const string _Urlprocesar_cambio_pagomovil = "/payment/action/v2-procesar-cambio-pagomovil";
+        const string _Urlquerystatus = "/payment/action/v2-querystatus";
+        public C2PMegasoftNav() {        
+            _UrlBase = "http://payment.somee.com";
+        }
+
+
+        public Preregister.response ExecutePreregister(Preregister.request valrequestObject) {
+            var requestXml = Serialize<Preregister.request>(valrequestObject);
+            var result = Post(_Urlpreregistr, requestXml);
+            if (!string.IsNullOrEmpty(result)) {
+                return Deserialize<Preregister.response>(result);
+            }
+            return null;
+        }
+
+        public ProcesarCambioPagoMovil.response ExecuteProcesarCambioPagoMovil(ProcesarCambioPagoMovil.request valrequestObject) {
+            var requestXml = Serialize<ProcesarCambioPagoMovil.request>(valrequestObject);
+            var result = Post(_Urlprocesar_cambio_pagomovil, requestXml);
+            if (!string.IsNullOrEmpty(result)) {
+                return Deserialize<ProcesarCambioPagoMovil.response>(result);
+            }
+            return null;
+        }
+
+        public Querystatus.response ExecuteQuerystatus(Querystatus.request valrequestObject) {
+            var requestXml = Serialize<Querystatus.request>(valrequestObject);
+            var result = Post(_Urlquerystatus, requestXml);
+            if (!string.IsNullOrEmpty(result)) {
+                return Deserialize<Querystatus.response>(result);
+            }
+            return null;
+        }
+
+        string Serialize<T>(T obj) {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            using (var sw = new StringWriter()) {
+                using (XmlTextWriter writer = new XmlTextWriter(sw) { Formatting = Formatting.Indented }) {
+                    serializer.Serialize(writer, obj);
+                    return sw.ToString();
+                }
+            }
+        }
+
+        T Deserialize<T>(string xml) {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            TextReader sr = new StringReader(xml);
+            return (T)serializer.Deserialize(sr);
+
+        }
+
+        private string Post(string Url, string requestXml) {
+            Uri baseUri = new Uri(_UrlBase);
+            var request = (HttpWebRequest)WebRequest.Create(new Uri(baseUri, Url));
+            byte[] bytes;
+            bytes = System.Text.Encoding.ASCII.GetBytes(requestXml);
+            request.ContentType = "application/xml";
+            request.ContentLength = bytes.Length;
+            request.Method = "POST";
+            Stream requestStream = request.GetRequestStream();
+            requestStream.Write(bytes, 0, bytes.Length);
+            requestStream.Close();
+            HttpWebResponse response;
+            response = (HttpWebResponse)request.GetResponse();
+            if (response.StatusCode == HttpStatusCode.OK) {
+                Stream responseStream = response.GetResponseStream();
+                string responseStr = new StreamReader(responseStream).ReadToEnd();
+                return responseStr;
+            }
+            return null;
+        }
+    }
+}
