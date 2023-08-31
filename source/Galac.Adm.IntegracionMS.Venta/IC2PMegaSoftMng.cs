847ee2fdff7e09b132df76c185568115cb957605
Mayelin Sánchez <Mayelin.Sanchez@galac.com>
2023-08-31 11:55:30 -0400

    se agrega interface para el manejo del view de vuelto con pago móvils, para abstraer el comportamiento en el proyecto de integración

---
+++ source/Galac.Adm.IntegracionMS.Venta/IC2PMegaSoftMng.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Galac.Adm.IntegracionMS.Venta {
+    public interface IC2PMegaSoftMng {
+        string NumeroControlVueltoPagoMovil { get; }
+        decimal MontoVueltoPagoMovil { get; }
+        void EjecutaVueltoPagoMovil(string valNombreCliente, string valNroFactura, decimal valMontoRestantePorPagar);
+    }
+}
