a701a65833aaad488d418f03fd1d1b30be24f023
luis.garcia <luis.garcia@galac.com>
2023-09-06 16:55:07 -0400

    Ajustes en métodos para procesar medios de pago

--- source/Galac.Adm.IntegracionMS.Venta/C2PMegasoftModel.cs
+++ source/Galac.Adm.IntegracionMS.Venta/C2PMegasoftModel.cs
@@ -56,8 +56,8 @@ namespace Galac.Adm.IntegracionMS.Venta {
         }
 
         public static class Constantes {
-            public const string valido = "00";
-            public const string invalido = "99";
+            //public const string valido = "00";
+            //public const string invalido = "99";
 
         }
     }
@@ -101,70 +101,5 @@ namespace Galac.Adm.IntegracionMS.Venta {
         }
     }
 
-    namespace ProcesarMetodoPago    {
-        public class request {
-            public string accion { get; set; }
-            public string montoTransaccion { get; set; }
-            public string cedula { get; set; }
-            public string tipoMoneda { get; set; }
-        }
-
-        public class response {
-            public string codRespuesta { get; set; }
-            public string mensajeRespuesta { get; set; }
-            public string nombreVoucher { get; set; }
-            public int numSeq { get; set; }
-            public string numeroTarjeta { get; set; }
-            public string cedula { get; set; }
-            public string montoTransaccion { get; set; }
-            public string montoAvance { get; set; }
-            public string montoServicios { get; set; }
-            public string montoServiciosAprobado { get; set; }
-            public string montoDonativo { get; set; }
-            public string tipoCuenta { get; set; }
-            public string tipoTarjeta { get; set; }
-            public string fechaExpiracion { get; set; }
-            public bool existeCopiaVoucher { get; set; }
-            public string fechaTransaccion { get; set; }
-            public string horaTransaccion { get; set; }
-            public string terminalVirtual { get; set; }
-            public string tipoTransaccion { get; set; }
-            public string numeroAutorizacion { get; set; }
-            public string codigoAfiliacion { get; set; }
-            public string tid { get; set; }
-            public string numeroReferencia { get; set; }
-            public string nombreAutorizador { get; set; }
-            public string codigoAdquiriente { get; set; }
-            public string numeroLote { get; set; }
-            public string tipoProducto { get; set; }
-            public string bancoEmisorCheque { get; set; }
-            public string numeroCuenta { get; set; }
-            public string numeroCheque { get; set; }
-            public string tipoMonedaFiat { get; set; }
-            public string descrMonedaFiat { get; set; }
-            public string montoCriptomoneda { get; set; }
-            public string tipoCriptomoneda { get; set; }
-            public string descrCriptomoneda { get; set; }
-            public string tipoMoneda { get; set; }
-            public string montoDivisa { get; set; }
-            public string descrMoneda { get; set; }
-            public string archivoCierre { get; set; }
-            public bool flagImpresion { get; set; }
-            public int medioPago { get; set; }
-            public string voucherServicios { get; set; }
-            public object listaTransServiciosOLB { get; set; }
-        }
-
-        //public class voucher
-        //{
-        //    public string linea { get; set; }
-        //}
-
-        //public static class Constantes
-        //{
-        //    public const string valido = "00";
-        //    public const string invalido = "99";
-
-        //}
-    }
+   
 }
--- source/Galac.Adm.IntegracionMS.Venta/C2PMegasoftNav.cs
+++ source/Galac.Adm.IntegracionMS.Venta/C2PMegasoftNav.cs
@@ -13,11 +13,8 @@ using Newtonsoft.Json;
 namespace Galac.Adm.IntegracionMS.Venta {
     public class C2PMegasoftNav {
         string _UrlBase;
-        //const string _Urlpreregistr = "payment/action/v2-preregistr";
-        //const string _Urlprocesar_cambio_pagomovil = "/payment/action/v2-procesar-cambio-pagomovil";
-        //const string _Urlquerystatus = "/payment/action/v2-querystatus";
-
-        const string _Urlprocesar_cambio_pagomovil = "/vpos/metodo";
+        const string respExitosa = "00";
+        const string _Urlprocesar_metodo_pago = "/vpos/metodo";
 
         public C2PMegasoftNav() {        
             //_UrlBase = "http://payment.somee.com";
@@ -36,21 +33,23 @@ namespace Galac.Adm.IntegracionMS.Venta {
                 };
                 ProcesarMetodoPago.response vResponse = SendProcesarMetodoDePago(request2);
                 if (vResponse != null) {
-                    if (vResponse.codRespuesta == ProcesarCambioPagoMovil.Constantes.valido) {
+                    if (vResponse.codRespuesta == respExitosa) {
                         vExito = true;
                     } else {
                         throw new LibGalac.Aos.Catching.GalacAlertException(vResponse.mensajeRespuesta);
+                        //LibGalac.Aos.UI.Mvvm.Messaging.LibMessages.RaiseError.ShowError(vResponse.mensajeRespuesta);
                     }
                 }
-            } catch {
-                throw new LibGalac.Aos.Catching.GalacAlertException("No se recibió respuesta. Por favor valide su conexión a internet. ");
+            } catch (System.Exception vEx) {
+                //throw new LibGalac.Aos.Catching.GalacAlertException("No se recibió respuesta. Por favor valide su conexión a internet. ");
+                LibGalac.Aos.UI.Mvvm.Messaging.LibMessages.MessageBox.Alert(null, vEx.Message, "Luis G.");
             }
             return vExito;
         }
 
         ProcesarMetodoPago.response SendProcesarMetodoDePago(ProcesarMetodoPago.request valrequestObject) {
             var requestJson = Serialize<ProcesarMetodoPago.request>(valrequestObject);
-            var result = Post(_Urlprocesar_cambio_pagomovil, requestJson);
+            var result = Post(_Urlprocesar_metodo_pago, requestJson);
             if (!string.IsNullOrEmpty(result)) {
                 return Deserialize<ProcesarMetodoPago.response>(result);
             }
@@ -85,5 +84,75 @@ namespace Galac.Adm.IntegracionMS.Venta {
         public static bool EsVisiblePM() {
             return false;
         }
+    }
+
+    namespace ProcesarMetodoPago
+    {
+        public class request
+        {
+            public string accion { get; set; }
+            public string montoTransaccion { get; set; }
+            public string cedula { get; set; }
+            public string tipoMoneda { get; set; }
+        }
+
+        public class response
+        {
+            public string codRespuesta { get; set; }
+            public string mensajeRespuesta { get; set; }
+            public string nombreVoucher { get; set; }
+            public int numSeq { get; set; }
+            public string numeroTarjeta { get; set; }
+            public string cedula { get; set; }
+            public string montoTransaccion { get; set; }
+            public string montoAvance { get; set; }
+            public string montoServicios { get; set; }
+            public string montoServiciosAprobado { get; set; }
+            public string montoDonativo { get; set; }
+            public string tipoCuenta { get; set; }
+            public string tipoTarjeta { get; set; }
+            public string fechaExpiracion { get; set; }
+            public bool existeCopiaVoucher { get; set; }
+            public string fechaTransaccion { get; set; }
+            public string horaTransaccion { get; set; }
+            public string terminalVirtual { get; set; }
+            public string tipoTransaccion { get; set; }
+            public string numeroAutorizacion { get; set; }
+            public string codigoAfiliacion { get; set; }
+            public string tid { get; set; }
+            public string numeroReferencia { get; set; }
+            public string nombreAutorizador { get; set; }
+            public string codigoAdquiriente { get; set; }
+            public string numeroLote { get; set; }
+            public string tipoProducto { get; set; }
+            public string bancoEmisorCheque { get; set; }
+            public string numeroCuenta { get; set; }
+            public string numeroCheque { get; set; }
+            public string tipoMonedaFiat { get; set; }
+            public string descrMonedaFiat { get; set; }
+            public string montoCriptomoneda { get; set; }
+            public string tipoCriptomoneda { get; set; }
+            public string descrCriptomoneda { get; set; }
+            public string tipoMoneda { get; set; }
+            public string montoDivisa { get; set; }
+            public string descrMoneda { get; set; }
+            public string archivoCierre { get; set; }
+            public bool flagImpresion { get; set; }
+            public int medioPago { get; set; }
+            public string voucherServicios { get; set; }
+            public object listaTransServiciosOLB { get; set; }
+        }
+
+        //public class voucher
+        //{
+        //    public string linea { get; set; }
+        //}
+
+        //public static class Constantes
+        //{
+        //    public const string valido = "00";
+        //    public const string invalido = "99";
+
+        //}
     }
 }
