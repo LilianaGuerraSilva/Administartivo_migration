a70c4395cf79a6769d6df236d48996ae1035ab6d
luis.garcia <luis.garcia@galac.com>
2023-09-06 13:58:29 -0400

    Se crea método para ejecutar Procesar Cambio Pago Movil

--- source/Galac.Adm.IntegracionMS.Venta/C2PMegasoftNav.cs
+++ source/Galac.Adm.IntegracionMS.Venta/C2PMegasoftNav.cs
@@ -12,111 +12,116 @@ using LibGalac.Aos.UI.Mvvm.Messaging;
 namespace Galac.Adm.IntegracionMS.Venta {
     public class C2PMegasoftNav {
         string _UrlBase;
-        const string _Urlpreregistr = "payment/action/v2-preregistr";
-        const string _Urlprocesar_cambio_pagomovil = "/payment/action/v2-procesar-cambio-pagomovil";
-        const string _Urlquerystatus = "/payment/action/v2-querystatus";
+        //const string _Urlpreregistr = "payment/action/v2-preregistr";
+        //const string _Urlprocesar_cambio_pagomovil = "/payment/action/v2-procesar-cambio-pagomovil";
+        //const string _Urlquerystatus = "/payment/action/v2-querystatus";
+
+        const string _Urlprocesar_cambio_pagomovil = "/vpos/metodo";
+
         public C2PMegasoftNav() {        
-            _UrlBase = "http://payment.somee.com";
+            //_UrlBase = "http://payment.somee.com";
+            _UrlBase = "http://localhost:8085";
         }
 
-        internal LibResponse EjecutaCambioPagoMovil(string valCedula, string valTelefono, string valCodigoBanco, string valVuelto, string valNroFactura, ref string refNumeroControl) {
-            LibResponse vResult = new LibResponse();
-            try {
-                string valCodigoAfiliacion = LibGlobalValues.Instance.GetAppMemInfo().GlobalValuesGetString("Parametros", "CodigoAfiliacionC2PMegasoft");
-                refNumeroControl = EjecutaPreRegistro(valCodigoAfiliacion);
-                if (!LibString.IsNullOrEmpty(refNumeroControl)) {
-                    vResult.Success  = EjecutaProcesarCambio(valCodigoAfiliacion, refNumeroControl, valCedula, valTelefono, valCodigoBanco, valVuelto, valNroFactura);
-                }
-            } catch (Exception ex) {
-                throw ex;
-            }
-            return vResult;
-        }
+        //internal LibResponse EjecutaCambioPagoMovil(string valCedula, string valTelefono, string valCodigoBanco, string valVuelto, string valNroFactura, ref string refNumeroControl) {
+        //    LibResponse vResult = new LibResponse();
+        //    try {
+        //        string valCodigoAfiliacion = LibGlobalValues.Instance.GetAppMemInfo().GlobalValuesGetString("Parametros", "CodigoAfiliacionC2PMegasoft");
+        //        refNumeroControl = EjecutaPreRegistro(valCodigoAfiliacion);
+        //        if (!LibString.IsNullOrEmpty(refNumeroControl)) {
+        //            vResult.Success  = EjecutaProcesarCambio(valCodigoAfiliacion, refNumeroControl, valCedula, valTelefono, valCodigoBanco, valVuelto, valNroFactura);
+        //        }
+        //    } catch (Exception ex) {
+        //        throw ex;
+        //    }
+        //    return vResult;
+        //}
 
-        string EjecutaPreRegistro(string valCodAfiliacion) {
-            string vResult;
-            try {
-                Preregister.request request = new Preregister.request() { cod_afiliacion = valCodAfiliacion };
-                Preregister.response vResponse = SendPreregister(request);
-                if (vResponse != null) {
-                    if (vResponse.codigo == Preregister.Constantes.valido) {
-                        vResult = vResponse.control;
-                    } else if (vResponse.codigo == Preregister.Constantes.invalido) {
-                        throw new LibGalac.Aos.Catching.GalacAlertException(vResponse.descripcion);
-                    } else {
-                        throw new LibGalac.Aos.Catching.GalacAlertException("No fue posible establecer conexión con el sistema de pago. Por favor intente nuevamente.");
-                    }
-                } else {
-                    throw new LibGalac.Aos.Catching.GalacAlertException("No fue posible establecer conexión con el sistema de pago. Por favor intente nuevamente.");
-                }
-            } catch {
-                throw new LibGalac.Aos.Catching.GalacAlertException("No fue posible establecer conexión con el sistema de pago. Por favor intente nuevamente.");
-            }
-            return vResult;
-        }
+        //string EjecutaPreRegistro(string valCodAfiliacion) {
+        //    string vResult;
+        //    try {
+        //        Preregister.request request = new Preregister.request() { cod_afiliacion = valCodAfiliacion };
+        //        Preregister.response vResponse = SendPreregister(request);
+        //        if (vResponse != null) {
+        //            if (vResponse.codigo == Preregister.Constantes.valido) {
+        //                vResult = vResponse.control;
+        //            } else if (vResponse.codigo == Preregister.Constantes.invalido) {
+        //                throw new LibGalac.Aos.Catching.GalacAlertException(vResponse.descripcion);
+        //            } else {
+        //                throw new LibGalac.Aos.Catching.GalacAlertException("No fue posible establecer conexión con el sistema de pago. Por favor intente nuevamente.");
+        //            }
+        //        } else {
+        //            throw new LibGalac.Aos.Catching.GalacAlertException("No fue posible establecer conexión con el sistema de pago. Por favor intente nuevamente.");
+        //        }
+        //    } catch {
+        //        throw new LibGalac.Aos.Catching.GalacAlertException("No fue posible establecer conexión con el sistema de pago. Por favor intente nuevamente.");
+        //    }
+        //    return vResult;
+        //}
 
-        bool EjecutaProcesarCambio(string valCodAfiliacion, string valCodigoControl, string valCedula, string valTelefono, string valCodigoBanco, string valVuelto, string valNroFactura) {
-            const string MonedaBs = "0";
-            bool vExito = false;
-            try {
-                ProcesarCambioPagoMovil.request request2 = new ProcesarCambioPagoMovil.request() {
-                    cod_afiliacion = valCodAfiliacion,
-                    control = valCodigoControl,
-                    cid = valCedula,
-                    telefono = valTelefono,
-                    codigobanco = valCodigoBanco,
-                    tipo_moneda = MonedaBs,
-                    amount = valVuelto,
-                    factura = valNroFactura
-                };
-                ProcesarCambioPagoMovil.response vResponse = SendProcesarCambioPagoMovil(request2);
-                if (vResponse != null) {
-                    if (vResponse.codigo == ProcesarCambioPagoMovil.Constantes.valido) {
-                        vExito = true;
-                    } else {
-                        throw new LibGalac.Aos.Catching.GalacAlertException(vResponse.descripcion);
-                    }
-                }
-            } catch {
-                throw new LibGalac.Aos.Catching.GalacAlertException("No se recibió respuesta. Por favor valide. Número de Control: " + valCodigoControl);
-            }
-            return vExito;
+        //bool EjecutaProcesarCambio(string valCodAfiliacion, string valCodigoControl, string valCedula, string valTelefono, string valCodigoBanco, string valVuelto, string valNroFactura) {
+        //    const string MonedaBs = "0";
+        //    bool vExito = false;
+        //    try {
+        //        ProcesarCambioPagoMovil.request request2 = new ProcesarCambioPagoMovil.request() {
+        //            cod_afiliacion = valCodAfiliacion,
+        //            control = valCodigoControl,
+        //            cid = valCedula,
+        //            telefono = valTelefono,
+        //            codigobanco = valCodigoBanco,
+        //            tipo_moneda = MonedaBs,
+        //            amount = valVuelto,
+        //            factura = valNroFactura
+        //        };
+        //        ProcesarCambioPagoMovil.response vResponse = SendProcesarCambioPagoMovil(request2);
+        //        if (vResponse != null) {
+        //            if (vResponse.codigo == ProcesarCambioPagoMovil.Constantes.valido) {
+        //                vExito = true;
+        //            } else {
+        //                throw new LibGalac.Aos.Catching.GalacAlertException(vResponse.descripcion);
+        //            }
+        //        }
+        //    } catch {
+        //        throw new LibGalac.Aos.Catching.GalacAlertException("No se recibió respuesta. Por favor valide. Número de Control: " + valCodigoControl);
+        //    }
+        //    return vExito;
+        //}
 
-        }
+        //Preregister.response SendPreregister(Preregister.request valrequestObject) {
+        //    var requestXml = Serialize<Preregister.request>(valrequestObject);
+        //    var result = Post(_Urlpreregistr, requestXml);
+        //    if (!string.IsNullOrEmpty(result)) {
+        //        return Deserialize<Preregister.response>(result);
+        //    }
+        //    return null;
+        //}
 
-        Preregister.response SendPreregister(Preregister.request valrequestObject) {
-            var requestXml = Serialize<Preregister.request>(valrequestObject);
-            var result = Post(_Urlpreregistr, requestXml);
-            if (!string.IsNullOrEmpty(result)) {
-                return Deserialize<Preregister.response>(result);
-            }
-            return null;
-        }
+        //ProcesarCambioPagoMovil.response SendProcesarCambioPagoMovil(ProcesarCambioPagoMovil.request valrequestObject) {
+        //    var requestXml = Serialize<ProcesarCambioPagoMovil.request>(valrequestObject);
+        //    var result = Post(_Urlprocesar_cambio_pagomovil, requestXml);
+        //    if (!string.IsNullOrEmpty(result)) {
+        //        return Deserialize<ProcesarCambioPagoMovil.response>(result);
+        //    }
+        //    return null;
+        //}
 
-        ProcesarCambioPagoMovil.response SendProcesarCambioPagoMovil(ProcesarCambioPagoMovil.request valrequestObject) {
-            var requestXml = Serialize<ProcesarCambioPagoMovil.request>(valrequestObject);
-            var result = Post(_Urlprocesar_cambio_pagomovil, requestXml);
-            if (!string.IsNullOrEmpty(result)) {
-                return Deserialize<ProcesarCambioPagoMovil.response>(result);
-            }
-            return null;
-        }
-
-        Querystatus.response SendQuerystatus(Querystatus.request valrequestObject) {
-            var requestXml = Serialize<Querystatus.request>(valrequestObject);
-            var result = Post(_Urlquerystatus, requestXml);
-            if (!string.IsNullOrEmpty(result)) {
-                return Deserialize<Querystatus.response>(result);
-            }
-            return null;
-        }
+        //Querystatus.response SendQuerystatus(Querystatus.request valrequestObject) {
+        //    var requestXml = Serialize<Querystatus.request>(valrequestObject);
+        //    var result = Post(_Urlquerystatus, requestXml);
+        //    if (!string.IsNullOrEmpty(result)) {
+        //        return Deserialize<Querystatus.response>(result);
+        //    }
+        //    return null;
+        //}
 
         string Post(string Url, string requestXml) {
             Uri baseUri = new Uri(_UrlBase);
             var request = (HttpWebRequest)WebRequest.Create(new Uri(baseUri, Url));
+            //var request = (HttpWebRequest)WebRequest.Create(baseUri);
             byte[] bytes;
             bytes = System.Text.Encoding.ASCII.GetBytes(requestXml);
-            request.ContentType = "application/xml";
+            //request.ContentType = "application/xml";
+            request.ContentType = "application/json";
             request.ContentLength = bytes.Length;
             request.Method = "POST";
             Stream requestStream = request.GetRequestStream();
@@ -151,6 +156,40 @@ namespace Galac.Adm.IntegracionMS.Venta {
 
         public static bool EsVisiblePM() {
             return false;
+        }
+
+
+        public bool EjecutaProcesarCambioPagoMovil(string valCedula, string valVuelto) {
+            const string MonedaBs = "VES";
+            bool vExito = false;
+            try {
+                ProcesarMetodoPago.request request2 = new ProcesarMetodoPago.request(){
+                   accion= "cambio" ,
+                   montoTransaccion = valVuelto,
+                   cedula = valCedula ,
+                   tipoMoneda = MonedaBs
+                };
+                ProcesarMetodoPago.response vResponse = SendProcesarMetodoDePago(request2);
+                if (vResponse != null) {
+                    if (vResponse.codRespuesta == ProcesarCambioPagoMovil.Constantes.valido) {
+                        vExito = true;
+                    } else {
+                        throw new LibGalac.Aos.Catching.GalacAlertException(vResponse.mensajeRespuesta);
+                    }
+                }
+            } catch {
+                throw new LibGalac.Aos.Catching.GalacAlertException("No se recibió respuesta. Por favor valide su conexión a internet. ");
+            }
+            return vExito;
+        }
+
+        ProcesarMetodoPago.response SendProcesarMetodoDePago(ProcesarMetodoPago.request valrequestObject) {
+            var requestXml = Serialize<ProcesarMetodoPago.request>(valrequestObject);
+            var result = Post(_Urlprocesar_cambio_pagomovil, requestXml);
+            if (!string.IsNullOrEmpty(result)) {
+                return Deserialize<ProcesarMetodoPago.response>(result);
+            }
+            return null;
         }
 
     }
