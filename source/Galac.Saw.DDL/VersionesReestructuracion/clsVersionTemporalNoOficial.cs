529fbedc0bff9c470947cb64df906f093b67a81a
erikson.didik <galac.softbb@galac.com>
2023-10-02 15:56:58 -0400

    se ajusta reestructuracion con la creacion de campos nuevos

--- source/Galac.Saw.DDL/VersionesReestructuracion/clsVersionTemporalNoOficial.cs
+++ source/Galac.Saw.DDL/VersionesReestructuracion/clsVersionTemporalNoOficial.cs
@@ -8,22 +8,22 @@ namespace Galac.Saw.DDL.VersionesReestructuracion {
 	class clsVersionTemporalNoOficial: clsVersionARestructurar {
 		public clsVersionTemporalNoOficial(string valCurrentDataBaseName) : base(valCurrentDataBaseName) { }
 		public override bool UpdateToVersion() {
-			StartConnectionNoTransaction();					
-			CamposMonedaExtranjeraEnCajaApertura();
+			StartConnectionNoTransaction();
+			CampoVueltoPagoMovilEnCajaApertura();
 			CamposInfoAdicionalRenglonCobroFactura();
 			DisposeConnectionNoTransaction();
 			return true;
 		}
 
-		private void CamposMonedaExtranjeraEnCajaApertura() {
+		private void CampoVueltoPagoMovilEnCajaApertura() {
 			if (AddColumnDecimal("Adm.CajaApertura", "MontoVueltoPM", 25, 4, "", 0)) {
 				AddDefaultConstraint("Adm.CajaApertura", "d_CajApeMoVuPM", "0", "MontoVueltoPM");
 			}
 		}
 
 		private void CamposInfoAdicionalRenglonCobroFactura() {
-			if (AddColumnString("RenglonCobroDeFactura", "InfoAdicional", 250,"",string.Empty)) {
-				AddDefaultConstraint("RenglonCobroDeFactura", "d_RenCobDeFacInAd", string.Empty, "InfoAdicional");
+			if (AddColumnString("renglonCobroDeFactura", "InfoAdicional", 250,"","")) {
+				AddDefaultConstraint("renglonCobroDeFactura", "d_RenCobDeFacInAd", "", "InfoAdicional");
 			}
 		}
 	}
