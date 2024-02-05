b9292b5b4daf50fe70d21f7a9121f011fe674865
romny.lopez <galac.softbb@galac.com>
2024-02-05 14:57:49 -0400

    se cambian referencias de libreriasa NuggetPackages

--- source/Galac.Saw.Uil.Cliente/Properties/AssemblyInfo.cs
+++ source/Galac.Saw.Uil.Cliente/Properties/AssemblyInfo.cs
@@ -37,5 +37,5 @@ using System.Windows;
     ResourceDictionaryLocation.ExternalAssembly, //where theme specific resource dictionaries are located
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
 )]
-[assembly: AssemblyVersion("2.60.5.0")]
-[assembly: AssemblyFileVersion("2.60.5.0")]
+[assembly: AssemblyVersion("2.61.0.0")]
+[assembly: AssemblyFileVersion("2.61.0.0")]
