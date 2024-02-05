b05c901e3b8a920d8e6bae76c357b65a34de1914
erikson.didik <galac.softbb@galac.com>
2024-02-05 16:32:26 -0400

    se suben cambios de referencias por cambio de version de Libreria

--- source/Galac.Adm.Uil.Vendedor/Properties/AssemblyInfo.cs
+++ source/Galac.Adm.Uil.Vendedor/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // Puede especificar todos los valores o establecer como predeterminados los números de versión de compilación y de revisión 
 // mediante el asterisco ('*'), como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.60.0.0")]
-[assembly: AssemblyFileVersion("2.60.0.0")]
+[assembly: AssemblyVersion("2.61.0.0")]
+[assembly: AssemblyFileVersion("2.61.0.0")]
