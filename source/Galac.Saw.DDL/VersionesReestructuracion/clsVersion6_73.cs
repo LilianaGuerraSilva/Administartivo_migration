ce4e9157530412f67415b1e03692be1ec52cab5e
erikson.didik <galac.softbb@galac.com>
2024-02-08 10:09:23 -0400

    se agrega la Clase de Reestructuracion correspondiente

---
+++ source/Galac.Saw.DDL/VersionesReestructuracion/clsVersion6_73.cs
@@ -0,0 +1,29 @@
+﻿using Galac.Adm.Dal.Vendedor;
+using Galac.Saw.Ccl.SttDef;
+using LibGalac.Aos.Base;
+using LibGalac.Aos.Base.Dal;
+using LibGalac.Aos.Brl;
+using LibGalac.Aos.Catching;
+using LibGalac.Aos.Dal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Galac.Saw.Ccl.Tablas;
+
+namespace Galac.Saw.DDL.VersionesReestructuracion {
+    class clsVersion6_73: clsVersionARestructurar {
+        public clsVersion6_73(string valCurrentDataBaseName) : base(valCurrentDataBaseName) { }
+        public override bool UpdateToVersion() {
+            StartConnectionNoTransaction();
+            AgregaParametroGeneral();
+            DisposeConnectionNoTransaction();
+            return true;
+        }
+        private void AgregaParametroGeneral() {
+            AgregarNuevoParametro("SuscripcionGVentas", "DatosGenerales", 1, "1.2.-General", 2, "", '2', "", 'N', "0");
+            AgregarNuevoParametro("SerialConectorGVentas", "DatosGenerales", 1, "1.2.-General", 2, "", '2', "", 'S', "");
+            AgregarNuevoParametro("NumeroIDGVentas", "DatosGenerales", 1, "1.2.-General", 2, "", '2', "", 'N', "");
+        }
+    }
+}
--- source/Galac.Saw.DDL/VersionesReestructuracion/clsVersionTemporalNoOficial.cs
+++ source/Galac.Saw.DDL/VersionesReestructuracion/clsVersionTemporalNoOficial.cs
@@ -10,7 +10,7 @@ namespace Galac.Saw.DDL.VersionesReestructuracion {
 	class clsVersionTemporalNoOficial: clsVersionARestructurar {
 		public clsVersionTemporalNoOficial(string valCurrentDataBaseName) : base(valCurrentDataBaseName) { }
 		public override bool UpdateToVersion() {
-			StartConnectionNoTransaction();	
+			StartConnectionNoTransaction();
 			DisposeConnectionNoTransaction();
 			return true;
 		}
