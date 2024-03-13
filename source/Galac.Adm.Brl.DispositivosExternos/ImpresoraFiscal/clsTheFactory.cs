2e9dc68e4aea22eaeb5a9c4274072c163d2fdcb8
romny.lopez <galac.softbb@galac.com>
2024-03-13 13:55:38 -0400

    se cprroge posicion de corte del campo direccion, se estaban perdiendo caracteres

--- source/Galac.Adm.Brl.DispositivosExternos/ImpresoraFiscal/clsTheFactory.cs
+++ source/Galac.Adm.Brl.DispositivosExternos/ImpresoraFiscal/clsTheFactory.cs
@@ -868,7 +868,7 @@ namespace Galac.Adm.Brl.DispositivosExternos.ImpresoraFiscal {
             if (valExtenderLineasAdicionales) {
                 vListaCMD.Add(GetLineaTexto() + LibText.Trim(LibText.SubString("Dir.:" + valDireccion, 0, _MaxLongitudDeTexto)));
                 if (!((_NumeroDeLineasDeTotalesEnDivisas + _LineaTextoAdicional) > 8)) {
-                    vListaCMD.Add(GetLineaTexto() + LibText.Trim(LibText.SubString(valDireccion, 30, _MaxLongitudDeTexto)));
+                    vListaCMD.Add(GetLineaTexto() + LibText.Trim(LibText.SubString(valDireccion, 35, _MaxLongitudDeTexto)));
                 }
                 if (!_ObservacionesAlFinalDeLaFactura) {
                     vListaCMD.Add(GetLineaTexto() + LibText.Trim(LibText.SubString("Obs.:" + valObservaciones, 0, _MaxLongitudDeTexto)));
@@ -883,7 +883,7 @@ namespace Galac.Adm.Brl.DispositivosExternos.ImpresoraFiscal {
                 if (!valDireccion.Equals("") && !_ModelosAntiguos) {
                     vListaCMD.Add(GetLineaTexto() + LibText.Trim(LibText.SubString("Dir.:" + valDireccion, 0, _MaxLongitudDeTexto)));
                     if (LibText.Len(LibText.Trim(valDireccion)) > 30) {
-                        vListaCMD.Add(GetLineaTexto() + LibText.Trim(LibText.SubString(valDireccion, 30, _MaxLongitudDeTexto)));
+                        vListaCMD.Add(GetLineaTexto() + LibText.Trim(LibText.SubString(valDireccion, 35, _MaxLongitudDeTexto)));
                     }
                 } else {
                     vListaCMD.Add(GetLineaTexto() + LibText.Trim(LibText.SubString("Dir.:" + valDireccion, 0, _MaxLongitudDeTexto)));
