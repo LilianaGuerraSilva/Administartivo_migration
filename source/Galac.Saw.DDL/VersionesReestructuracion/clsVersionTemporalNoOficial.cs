c02f96e862f2334bb20664e8934bdd6541541dc2
romny.lopez <galac.softbb@galac.com>
2024-07-16 09:28:37 -0400

    se agrega construccion de parametros de gVentas para empresas ya creadas de la version anterior

--- source/Galac.Saw.DDL/VersionesReestructuracion/clsVersionTemporalNoOficial.cs
+++ source/Galac.Saw.DDL/VersionesReestructuracion/clsVersionTemporalNoOficial.cs
@@ -13,9 +13,18 @@ namespace Galac.Saw.DDL.VersionesReestructuracion {
 	class clsVersionTemporalNoOficial : clsVersionARestructurar {
 		public clsVersionTemporalNoOficial(string valCurrentDataBaseName) : base(valCurrentDataBaseName) { }
 		public override bool UpdateToVersion() {
-			StartConnectionNoTransaction();	
-			DisposeConnectionNoTransaction();
+			StartConnectionNoTransaction();
+            AgregaParametroGeneral();
+            DisposeConnectionNoTransaction();
 			return true;
 		}
-	}
+
+        private void AgregaParametroGeneral() {
+            AgregarNuevoParametro("SuscripcionGVentas", "DatosGenerales", 1, "1.2.-General", 2, "", '2', "", 'S', "1000;1001;1002;1003;1004;1005");
+            AgregarNuevoParametro("SerialConectorGVentas", "DatosGenerales", 1, "1.2.-General", 2, "", '2', "", 'S', "");
+            AgregarNuevoParametro("NumeroIDGVentas", "DatosGenerales", 1, "1.2.-General", 2, "", '2', "", 'N', "");
+        }
+
+    }
+
 }
