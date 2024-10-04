56e318dbeed6a372320074dea4136ba230f85470
Jose.Colmenarez <galac.softbb@galac.com>
2024-10-04 11:30:21 -0400

    ajuste por pruebas de QA

---
+++ source/Galac.Adm.Uil.GestionProduccion/OrdenDeProduccionMessagesHandler.cs
@@ -0,0 +1,27 @@
+﻿using Galac.Adm.Ccl.GestionProduccion;
+using Galac.Adm.Uil.GestionProduccion.ViewModel;
+using LibGalac.Aos.Base;
+using LibGalac.Aos.Brl;
+using LibGalac.Aos.UI.Mvvm.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Galac.Adm.Uil.GestionProduccion {
+    public class OrdenDeProduccionMessagesHandler {
+        static OrdenDeProduccionMessagesHandler _Default = new OrdenDeProduccionMessagesHandler();
+
+        public static void RegisterMessages() {
+            LibBusinessProcess.Register(_Default, "ConsultarOrdenDeProduccion", EjecutarConsultarOrdenDeProduccion);
+
+        }
+
+        private static void EjecutarConsultarOrdenDeProduccion(LibBusinessProcessMessage valMessage) {
+            int _Consecutivo = (int)valMessage.Content;
+            OrdenDeProduccionViewModel vViewModel = new OrdenDeProduccionViewModel(new OrdenDeProduccion() { ConsecutivoCompania = LibGlobalValues.Instance.GetMfcInfo().GetInt("Compania"), Consecutivo = _Consecutivo }, eAccionSR.Consultar);
+            vViewModel.InitializeViewModel(eAccionSR.Consultar);
+            LibMessages.EditViewModel.ShowEditor(vViewModel, true);
+        }
+    }
+}
