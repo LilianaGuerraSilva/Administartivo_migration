c5a47e8dfacf16f13505c57955ac7892756091c8
romny.lopez <galac.softbb@galac.com>
2024-11-21 16:50:24 -0400

    se corrige nombre de parametro

--- source/Galac.Saw.DDL/VersionesReestructuracion/clsVersionTemporalNoOficial.cs
+++ source/Galac.Saw.DDL/VersionesReestructuracion/clsVersionTemporalNoOficial.cs
@@ -1,21 +1,19 @@
 using Galac.Saw.Ccl.SttDef;
 using LibGalac.Aos.Base;
 
-namespace Galac.Saw.DDL.VersionesReestructuracion
-{
+namespace Galac.Saw.DDL.VersionesReestructuracion {
 
     class clsVersionTemporalNoOficial : clsVersionARestructurar {
-		public clsVersionTemporalNoOficial(string valCurrentDataBaseName) : base(valCurrentDataBaseName) { }
-		public override bool UpdateToVersion() {
-			StartConnectionNoTransaction();
+        public clsVersionTemporalNoOficial(string valCurrentDataBaseName) : base(valCurrentDataBaseName) { }
+        public override bool UpdateToVersion() {
+            StartConnectionNoTransaction();
             CrearParametrosCreditoElectronico();
             DisposeConnectionNoTransaction();
-			return true;
-		}
+            return true;
+        }
 
-        private void CrearParametrosCreditoElectronico()
-        {
-            AgregarNuevoParametro("UsaCreditoEelectronico", "Factura", 2, "2.2.- Facturación (Continuación) ", 2, "", eTipoDeDatoParametros.String, "", 'N', "N");
+        private void CrearParametrosCreditoElectronico() {
+            AgregarNuevoParametro("UsaCreditoElectronico", "Factura", 2, "2.2.- Facturación (Continuación) ", 2, "", eTipoDeDatoParametros.String, "", 'N', "N");
             AgregarNuevoParametro("NombreCreditoElectronico", "Factura", 2, "2.2.- Facturación (Continuación) ", 2, "", eTipoDeDatoParametros.String, "", 'N', "Crédito Electrónico");
             AgregarNuevoParametro("DiasUsualesCreditoElectronico", "Factura", 2, "2.2.- Facturación (Continuación) ", 2, "", eTipoDeDatoParametros.Int, "", 'N', "14");
             AgregarNuevoParametro("DiasMaximoCreditoElectronico", "Factura", 2, "2.2.- Facturación (Continuación) ", 2, "", eTipoDeDatoParametros.Int, "", 'N', "14");
