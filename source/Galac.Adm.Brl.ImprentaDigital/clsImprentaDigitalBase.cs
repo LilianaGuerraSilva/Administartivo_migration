597c9074b2f8742e055ac8dc60da38638bfda696
bqto-02\romny.lopez <romny.lopez@galac.com>
2025-02-12 22:41:31 -0400

    se corrige regex

--- source/Galac.Adm.Brl.ImprentaDigital/clsImprentaDigitalBase.cs
+++ source/Galac.Adm.Brl.ImprentaDigital/clsImprentaDigitalBase.cs
@@ -519,8 +519,8 @@ namespace Galac.Adm.Brl.ImprentaDigital {
             valInput = LibString.Trim(valInput);
             System.Text.RegularExpressions.Regex vListInvalidChars = new System.Text.RegularExpressions.Regex("[´|`|~|^|¨|'|\n|\r|\t]", System.Text.RegularExpressions.RegexOptions.Compiled);
             vResult = vListInvalidChars.Replace(valInput, "");
-            vListInvalidChars = new System.Text.RegularExpressions.Regex("Total", System.Text.RegularExpressions.RegexOptions.None);
-            vResult = vListInvalidChars.Replace(valInput, "");
+            vListInvalidChars = new System.Text.RegularExpressions.Regex("(?i)total");
+            vResult = vListInvalidChars.Replace(vResult, "");
             return vResult;
         }
 
