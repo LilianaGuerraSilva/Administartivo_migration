390de7eb1dc982f5926c56f4b29b2f1b5826c1db
romny.lopez <galac.softbb@galac.com>
2025-02-19 13:40:44 -0400

    se incluten parametros faltantes

--- source/Galac.Saw.Ccl.SttDef/ImprentaDigitalDatosDeConexion.cs
+++ source/Galac.Saw.Ccl.SttDef/ImprentaDigitalDatosDeConexion.cs
@@ -47,7 +47,7 @@ namespace Galac.Saw.Ccl.SttDef {
 
         public string Clave {
             get { return _Clave; }
-            set { _Clave = LibString.Mid(value, 0, 100); }
+            set { _Clave = LibString.Mid(value, 0, 1000); }
         }
         #endregion //Propiedades
         #region Constructores
