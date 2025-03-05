0869e64c7548ed58944db845cb6fa047509c055e
David Hurtado <david.hurtado@galac.com>
2025-03-05 14:15:34 -0400

    feat: Se agregan los método de version homologada.

--- source/Galac.Saw.Brl.SttDef/clsSettValueByCompanyNav.cs
+++ source/Galac.Saw.Brl.SttDef/clsSettValueByCompanyNav.cs
@@ -2701,5 +2701,13 @@ namespace Galac.Saw.Brl.SttDef {
             }
             return vResult;
         }
+        string ISettValueByCompanyPdn.VersionHomologada(string valVesionActual) {
+            try {
+                LibWebConnector.clsComprobarVersion  insComprobarVersion = new LibWebConnector.clsComprobarVersion();
+                return insComprobarVersion.ObtenerVersionHomologada();
+            } catch (Exception vEx) {
+                throw new GalacException(vEx.Message, eExceptionManagementType.Controlled);
+            }
+        }
     } //End of class clsSettValueByCompanyNav
 } //End of namespace Galac.Saw.Brl.PrdStt
